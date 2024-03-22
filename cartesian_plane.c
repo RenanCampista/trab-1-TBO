fba2e672e2b4ae4cc61ee31cae0913d523c8a741
Renan Campista <renan.campistah@hotmail.com>
2024-03-22 08:17:35 -0300

    Função para leitura dos pontos no plano cartesiano

--- cartesian_plane.c
+++ cartesian_plane.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 #include "cartesian_plane.h"
 #include "vector.h"
 #include "point.h"
@@ -34,6 +36,38 @@ void cartesian_plane_destroy(CartesianPlane *cp) {
     free(cp);
 }
 
+void cartesian_plane_read(CartesianPlane *cp, FILE *input) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if ((read = getline(&line, &len, input)) == -1)
+        exit(printf("Error: input file is empty.\n"));
+
+    // Remove o caractere de nova linha (\n) do final da linha. Evitar realloc.
+    line[strcspn(line, "\n")] = 0;
+    for (int i = 0; line[i] != '\0'; i++) {
+        if (line[i] == ',') 
+            cp->dimension++;    
+    }
+    // Retorna ao início do arquivo para processar novamente
+    fseek(input, 0, SEEK_SET);
+
+    while((read = getline(&line, &len, input)) != -1) {
+        // Remove o caractere \n do final da linha
+        line[strcspn(line, "\n")] = 0;
+        //Pode retirar size da struct point
+        Point *p = point_read(cp->dimension, line);
+        vector_push_back(cp->points, p);
+        // point_print(p);
+        // point_destroy(p);
+    }
+
+    cp->euclidean_dist = (double **)calloc(vector_size(cp->points), sizeof(double *));
+    for (int i = 0; i < vector_size(cp->points); i++) 
+        cp->euclidean_dist[i] = (double *)calloc(vector_size(cp->points), sizeof(double));
+}
+
 void cartesian_plane_calculate_distances(CartesianPlane *cp) {
     for (int i = 0; i < vector_size(cp->points); i++) {
         for (int j = 0; j < i; j++) {
@@ -43,3 +77,4 @@ void cartesian_plane_calculate_distances(CartesianPlane *cp) {
         }
     }
 }
+
