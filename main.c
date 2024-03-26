9a1cbc0bcfc11ed29fb5d8045bb7cd53cb1088f2
Renan Campista <renan.campistah@hotmail.com>
2024-03-26 19:02:34 -0300

    inserindo marcação de tempo do programa com time.h

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "cartesian_plane.h"
 #include "kruskal.h"
 
 int main(int argc, char *argv[]) {
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+
     //./trab1 <nome_arquivo_entrada> k <nome_arquivo_saida>
     if (argc != 4)
         exit(printf("Error: invalid number of arguments.\n"));
@@ -18,6 +24,15 @@ int main(int argc, char *argv[]) {
   
     kruskal_solve(cp, groups, argv[3]);
     cartesian_plane_destroy(cp);
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    FILE *time_file = fopen("runtime.txt", "w");
+    if (time_file == NULL)
+        exit(printf("Error: could not open runtime file.\n"));
+    fprintf(time_file, "%.3f\n", cpu_time_used);
+    fclose(time_file);
 
     return 0;
 }
